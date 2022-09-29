c2760c5c283be604d7a700467e570018e47b0619
Joshua Brooks <joshua@letsleapforward.com>
2022-09-29 15:22:37 +1000

    Update node test description

--- Challenge1/index.js
+++ Challenge1/index.js
@@ -1,7 +1,12 @@
 /*
-Your first challenge is to generate an env file
+Your first challenge is to write a function which generates an env file
 The file name should be .development.env
-You need to get the values from startingValues.json and add all of them to the env file
+You need to get the values from startingValues.json and add all of them to the env file when it is being generated
+
+So the end result in the env file should look something like (but will have different values):
+PORT=3000
+HOST=localhost
+DB_HOST=localhost
 */
 
 // Write your code here:
--- Challenge2/index.js
+++ Challenge2/index.js
@@ -6,17 +6,16 @@ See API docs here: https://github.com/r-spacex/SpaceX-API/tree/master/docs#rspac
 
 const { default: axios } = require("axios");
 
-// Write your code here:
+// Write your code here, I've gotten it started for you:
 const run = async () => {
   const data = await axios.get("https://api.spacexdata.com/v4/launches");
 
   return data;
 };
 
-run().then((data) => console.log(data));
-
-// Below console.log() all of the launches
+// Below console.log() an array of the launch.details for each launch
 console.log("All Launches");
 
-// Below console.log() all of the launch Ids within the last two months
+// Below console.log() all of the launches within the last 60 days
+// Log only the launch Ids and launch dates formatted as an dd/mm/yyyy
 console.log("Recent Launches");
